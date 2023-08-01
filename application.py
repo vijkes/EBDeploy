a08be9c2616bec6e77900640f2730ab7a96b02d0
saurabhgarg013 <46773208+saurabhgarg013@users.noreply.github.com>
2023-08-01 19:46:29 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return "<h1>Hello, this is a simple Flask web application and deply by Code pipeline!</h1>"
+    return "<h1>Hello, this is a simple Flask web application and deploy by Code pipeline!</h1>"
 
 if __name__ == '__main__':
     application.run()
